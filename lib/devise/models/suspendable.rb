27f3df06ea77fc939e9ab2fffeb04324522cb6f6
neilha <neilharrold@gmail.com>
2024-05-10 10:55:35 +0100

    adds devise suspendable module

---
+++ lib/devise/models/suspendable.rb
@@ -0,0 +1,48 @@
+module Devise
+  module Models
+    module Suspendable
+      extend ActiveSupport::Concern
+
+      included do
+        # active scope is returning all users, including those
+        # where suspend_at = nil...
+        # using User.not_suspended for now
+        # TODO: work out why this scope is not working
+        #       but the one in user model is.
+        scope :active, -> { where("suspended_at" => nil) }
+        scope :suspended, -> { where("suspended_at IS NOT NULL") }
+        scope(:current, proc { |current| current == "t" ? active : suspended })
+      end
+
+      def active_for_authentication?
+        if super && !suspended?
+          return true
+        end
+
+        false
+      end
+
+      def inactive_message
+        suspended? ? :suspended : super
+      end
+
+      # Return value is checked, so don't raise
+      # error on validation failures
+      def suspend(reason)
+        update(reason_for_suspension: reason,
+               suspended_at: Time.zone.now)
+      end
+
+      def unsuspend
+        update(password: SecureRandom.hex,
+               unsuspended_at: Time.zone.now,
+               suspended_at: nil,
+               reason_for_suspension: nil)
+      end
+
+      def suspended?
+        suspended_at.present?
+      end
+    end
+  end
+end
