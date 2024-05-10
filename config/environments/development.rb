c25f69942365fdaae919df575a26d58179ada7ed
neilha <neilharrold@gmail.com>
2024-05-10 10:01:10 +0100

    database setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,9 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+  # Allow requests for all domains e.g. <app>.dev.publishing-platform.co.uk
+  config.hosts.clear  
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
