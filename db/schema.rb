9b4d13ea4c051c9e2db20db0a8d645ac3b26d84a
Neil Harrold <neilharrold@gmail.com>
2024-05-22 14:22:39 +0100

    updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,55 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_13_211525) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_22_130318) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "oauth_access_grants", force: :cascade do |t|
+    t.bigint "resource_owner_id", null: false
+    t.bigint "application_id", null: false
+    t.string "token", null: false
+    t.integer "expires_in", null: false
+    t.text "redirect_uri", null: false
+    t.string "scopes", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "revoked_at"
+    t.index ["application_id"], name: "index_oauth_access_grants_on_application_id"
+    t.index ["resource_owner_id"], name: "index_oauth_access_grants_on_resource_owner_id"
+    t.index ["token"], name: "index_oauth_access_grants_on_token", unique: true
+  end
+
+  create_table "oauth_access_tokens", force: :cascade do |t|
+    t.bigint "resource_owner_id"
+    t.bigint "application_id", null: false
+    t.string "token", null: false
+    t.string "refresh_token"
+    t.integer "expires_in"
+    t.string "scopes"
+    t.datetime "created_at", null: false
+    t.datetime "revoked_at"
+    t.string "previous_refresh_token", default: "", null: false
+    t.index ["application_id"], name: "index_oauth_access_tokens_on_application_id"
+    t.index ["refresh_token"], name: "index_oauth_access_tokens_on_refresh_token", unique: true
+    t.index ["resource_owner_id"], name: "index_oauth_access_tokens_on_resource_owner_id"
+    t.index ["token"], name: "index_oauth_access_tokens_on_token", unique: true
+  end
+
+  create_table "oauth_applications", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "uid", null: false
+    t.string "secret", null: false
+    t.text "redirect_uri", null: false
+    t.string "scopes", default: "", null: false
+    t.boolean "confidential", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "home_uri"
+    t.string "description"
+    t.boolean "retired", default: false
+    t.index ["name"], name: "index_oauth_applications_on_name", unique: true
+    t.index ["uid"], name: "index_oauth_applications_on_uid", unique: true
+  end
 
   create_table "organisations", force: :cascade do |t|
     t.string "content_id", null: false
@@ -25,6 +71,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_13_211525) do
     t.datetime "updated_at", null: false
     t.index ["content_id"], name: "index_organisations_on_content_id", unique: true
     t.index ["slug"], name: "index_organisations_on_slug", unique: true
+  end
+
+  create_table "permissions", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "oauth_application_id"
+    t.index ["oauth_application_id", "name"], name: "index_permissions_on_oauth_application_id_and_name", unique: true
+    t.index ["oauth_application_id"], name: "index_permissions_on_oauth_application_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -75,5 +130,19 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_13_211525) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  create_table "users_permissions", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "permission_id", null: false
+    t.index ["permission_id", "user_id"], name: "index_users_permissions_on_permission_id_and_user_id"
+    t.index ["user_id", "permission_id"], name: "index_users_permissions_on_user_id_and_permission_id"
+  end
+
+  add_foreign_key "oauth_access_grants", "oauth_applications", column: "application_id"
+  add_foreign_key "oauth_access_grants", "users", column: "resource_owner_id"
+  add_foreign_key "oauth_access_tokens", "oauth_applications", column: "application_id"
+  add_foreign_key "oauth_access_tokens", "users", column: "resource_owner_id"
+  add_foreign_key "permissions", "oauth_applications"
   add_foreign_key "users", "organisations"
+  add_foreign_key "users_permissions", "permissions"
+  add_foreign_key "users_permissions", "users"
 end
