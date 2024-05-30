7debb0fa921c495d9ade89528f8c0c029aeffa4f
neilha <neilharrold@gmail.com>
2024-05-30 18:01:26 +0100

    removes tom-select

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,6 @@
 // Entry point for the build script in your package.json
 import "bootstrap/dist/js/bootstrap.bundle.min.js"
 import CopyToClipboard from './components/copy-to-clipboard'
-import TomSelect from 'tom-select'
 
 const $copyToClipboard = document.querySelector('[data-module="copy-to-clipboard"]')
 if ($copyToClipboard) {
