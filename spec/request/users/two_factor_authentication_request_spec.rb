c411e6cbb8ec5654620fd877a4625f8070c9551d
Neil Harrold <neilharrold@gmail.com>
2024-11-20 15:00:15 +0000

    2fa request specs

---
+++ spec/request/users/two_factor_authentication_request_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+RSpec.describe "User two factor authentication", type: :request do
+  let(:user) { create(:user) }
+
+  describe "GET prompt" do
+    context "when user is not authenticated" do
+      it "redirects user to sign in" do
+        get prompt_two_factor_authentication_path
+
+        assert_not_authenticated
+      end
+    end
+  end
+
+  describe "GET show" do
+    context "when user is not authenticated" do
+      it "redirects user to sign in" do
+        get two_factor_authentication_path
+
+        assert_not_authenticated
+      end
+    end
+  end  
+
+  describe "PUT update" do
+    context "when user is not authenticated" do
+      it "redirects user to sign in" do
+        put two_factor_authentication_path
+
+        assert_not_authenticated
+      end
+    end
+  end    
+end
---
+++ spec/request/users/two_factor_authentication_session_request_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "User two factor authentication session", type: :request do
+  let(:user) { create(:user) }
+
+  describe "GET new" do
+    context "when user is not authenticated" do
+      it "redirects user to sign in" do
+        get new_two_factor_authentication_session_path
+
+        assert_not_authenticated
+      end
+    end
+  end
+
+  describe "POST create" do
+    context "when user is not authenticated" do
+      it "redirects user to sign in" do
+        post two_factor_authentication_session_path
+
+        assert_not_authenticated
+      end
+    end
+  end 
+end
