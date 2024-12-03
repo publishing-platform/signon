9f0f89a1edcff2ee97307604a8515e47581137a4
neilha <neilharrold@gmail.com>
2024-12-03 14:53:22 +0000

    continues work on  users request specs

--- spec/request/users_request_spec.rb
+++ spec/request/users_request_spec.rb
@@ -194,8 +194,171 @@ RSpec.describe "Users", type: :request do
         assert_select "select[name='user[organisation_id]']"
       end
 
+      it "renders a link to suspend user" do
+        another_user = create(:user)
+
+        get edit_user_path(another_user)
+
+        assert_select "a[href='#{edit_user_suspensions_path(another_user)}']", text: "Suspend user"
+      end
+
+      it "renders a link to unsuspend suspended user" do
+        suspended_user = create(:suspended_user)
+
+        get edit_user_path(suspended_user)
+
+        assert_select "a[href='#{edit_user_suspensions_path(suspended_user)}']", text: "Unsuspend user"
+      end
+
+      it "renders a form to resend invitation for a user who has been invited but has not accepted" do
+        invited_user = create(:invited_user)
+
+        get edit_user_path(invited_user)
+
+        assert_select "form[action='#{resend_user_invitation_path(invited_user)}'] input[type='submit'][value='Resend signup email']"
+      end
+
+      it "renders a form to unlock account for a user who has been locked out" do
+        locked_user = create(:locked_user)
+
+        get edit_user_path(locked_user)
+
+        assert_select "form[action='#{unlock_user_path(locked_user)}'] input[type='submit'][value='Unlock account']"
+      end
+
+      it "renders a form to resend confirmation email for a user who has a pending email change" do
+        another_user = create(:user, unconfirmed_email: "unconfirmed@email.com")
+
+        get edit_user_path(another_user)
+
+        assert_select "form[action='#{resend_email_change_user_path(another_user)}'] input[type='submit'][value='Resend confirmation email']"
+      end
+
+      it "renders a form to cancel email change for a user who has a pending email change" do
+        another_user = create(:user, unconfirmed_email: "unconfirmed@email.com")
+
+        get edit_user_path(another_user)
+
+        assert_select "form[action='#{cancel_email_change_user_path(another_user)}'] input[type='submit'][value='Cancel email change']"
+      end
+
+      it "renders a form to reset 2FA" do
+        another_user = create(:user, otp_secret: "secret")
+
+        get edit_user_path(another_user)
+
+        assert_select "form[action='#{reset_2fa_user_path(another_user)}'] input[type='submit'][value='Reset 2FA']"
+      end
+
       it "sets not found status code if the user does not exist" do
         get edit_user_path(user, { id: "non-existent-user-id" })
+
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+  end
+
+  describe "PATCH update" do
+    context "when user is not authenticated" do
+      it "redirects user to sign in" do
+        another_user = create(:user)
+
+        patch user_path(another_user)
+
+        assert_not_authenticated
+      end
+    end
+
+    context "when user is a normal user" do
+      before do
+        sign_in user
+      end
+
+      after do
+        sign_out user
+      end
+
+      it "does not allow access" do
+        another_user = create(:user)
+
+        patch user_path(another_user)
+
+        assert_not_authorised
+      end
+    end
+
+    context "when user is an admin user" do
+      before do
+        user.update!(role: "admin")
+        sign_in user
+      end
+
+      after do
+        sign_out user
+      end
+
+      it "redirects and displays success message" do
+        another_user = create(:user)
+
+        patch user_path(another_user), params: { user: { name: "Joe Bloggs" } }
+
+        expect(response).to redirect_to(users_path)
+        follow_redirect!
+
+        expect(response.body).to include("Updated user #{another_user.email} successfully")
+      end
+
+      it "updates user" do
+        another_user = create(:user)
+
+        patch user_path(another_user), params: { user: { name: "Joe Bloggs" } }
+
+        expect(another_user.reload.name).to eql("Joe Bloggs")
+      end
+
+      it "causes new invitation email to be sent if email is updated and user is invited but has not yet accepted" do
+        invited_user = create(:invited_user)
+        new_email = "joe@email.com"
+
+        patch user_path(invited_user), params: { user: { email: new_email } }
+
+        expect(invited_user.reload.email).to eql(new_email)
+        expect(last_email.to).to contain_exactly(new_email)
+        expect(last_email.subject).to eql "Invitation instructions"
+      end
+
+      it "displays error and does not update user if name is not provided" do
+        another_user = create(:user, name: "Joe Bloggs")
+
+        patch user_path(another_user), params: { user: { name: "" } }
+
+        expect(response.body).to include("Name can't be blank")
+
+        expect(another_user.reload.name).to eql("Joe Bloggs")
+      end
+
+      it "displays error and does not update user if email is not provided" do
+        another_user = create(:user, email: "joe@email.com")
+
+        patch user_path(another_user), params: { user: { email: "" } }
+
+        expect(response.body).to include("Email can't be blank")
+
+        expect(another_user.reload.email).to eql("joe@email.com")
+      end
+
+      it "displays error and does not update user if role is not provided" do
+        another_user = create(:user, email: "joe@email.com")
+
+        patch user_path(another_user), params: { user: { role: "" } }
+
+        expect(response.body).to include("Role is not included in the list")
+
+        expect(another_user.reload.role).to eql("normal")
+      end
+
+      it "sets not found status code if the user does not exist" do
+        patch user_path(user, { id: "non-existent-user-id" })
 
         expect(response).to have_http_status(:not_found)
       end
