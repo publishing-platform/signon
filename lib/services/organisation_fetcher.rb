c61ddea67eb0c986e4265e64e36150952835f85a
neilha <neilharrold@gmail.com>
2025-02-13 16:47:57 +0000

    retrieve organisations from organisations api (collections-frontend)

--- lib/services/organisation_fetcher.rb
+++ lib/services/organisation_fetcher.rb
@@ -9,44 +9,12 @@ module Services
   private
 
     def organisations
-      # call API here to fetch organisations
-      [
-        {
-          title: "Research and Development",
-          format: "Department",
-          details: {
-            slug: "research-and-development",
-            abbreviation: nil,
-            status: "live",
-            content_id: "96ae61d6-c2a1-48cb-8e67-da9d105ae381",
-          },
-        },
-        {
-          title: "Digital Services",
-          format: "Department",
-          details: {
-            slug: "digital-services",
-            abbreviation: "DS",
-            status: "live",
-            content_id: "af07d5a5-df63-4ddc-9383-6a666845ebe9",
-          },
-        },
-        {
-          title: "Financial Services",
-          format: "Department",
-          details: {
-            slug: "financial-services",
-            abbreviation: "FS",
-            status: "live",
-            content_id: "06056197-bc69-4147-aa28-070bca132178",
-          },
-        },
-      ]
+      @organisations ||= PublishingPlatformApi.organisations.organisations.with_subsequent_pages
     end
 
     def update_or_create_organisation(organisation_data)
-      content_id = organisation_data[:details][:content_id]
-      slug = organisation_data[:details][:slug]
+      content_id = organisation_data["details"]["content_id"]
+      slug = organisation_data["details"]["slug"]
 
       organisation = Organisation.find_by(content_id:) ||
         Organisation.find_by(slug:) ||
@@ -55,10 +23,10 @@ module Services
       update_data = {
         content_id:,
         slug:,
-        name: organisation_data[:title],
-        organisation_type: organisation_data[:format],
-        abbreviation: organisation_data[:details][:abbreviation],
-        closed: organisation_data[:details][:status] == "closed",
+        name: organisation_data["title"],
+        organisation_type: organisation_data["format"],
+        abbreviation: organisation_data["details"]["abbreviation"],
+        closed: organisation_data["details"]["status"] == "closed",
       }
 
       organisation.update!(update_data)
