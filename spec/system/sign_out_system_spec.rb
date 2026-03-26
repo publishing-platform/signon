34f383dc08e9895da792bd2b09ad5f05fad0bee3
neilha <neilharrold@gmail.com>
2026-03-26 21:41:12 +0000

    continues refactoring system tests to feature tests.

--- spec/system/sign_out_system_spec.rb
+++
@@ -1,8 +0,0 @@
-require "rails_helper"
-
-RSpec.describe "Sign out", type: :system do
-  it "does not blow up if not already signed in" do
-    signout
-    expect(page).to have_text("Sign in to Publishing Platform")
-  end
-end
